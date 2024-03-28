60801db3cd5ce57cbae2e9c2325c6664c5b6da11
bong <darksen7693@gmail.com>
2024-03-28 15:07:35 +0900

    family

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:actual/layout/default_layout.dart';
+import 'package:actual/screen/family_modifier_screen.dart';
 import 'package:actual/screen/future_provider_screen.dart';
 import 'package:actual/screen/state_notifier_provider_screen.dart';
 import 'package:actual/screen/state_provider_screen.dart';
@@ -53,6 +54,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: Text("StreamProviderScreen"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => FamilyModifierScreen(),
+                ),
+              );
+            },
+            child: Text("FamilyModifierScreen"),
           )
         ],
       ),
