fd6d2bcd58179d5c6dbde887ff2e9546baee4f10
bong <darksen7693@gmail.com>
2024-03-29 10:43:49 +0900

    Keep Alive

--- lib/riverpod/code_generation_provider.dart
+++ lib/riverpod/code_generation_provider.dart
@@ -14,4 +14,21 @@ String gState(GStateRef ref) {
   return "Hello Code Generation";
 }
 
+@riverpod
+Future<int> gStateFuture(GStateFutureRef ref) async {
+  await Future.delayed(Duration(seconds: 3));
+
+  return 10;
+}
+
+@Riverpod(
+  //살려두는거 기본값 false (autodispose)
+  keepAlive: true,
+)
+Future<int> gStateFuture2(GStateFuture2Ref ref) async {
+  await Future.delayed(Duration(seconds: 3));
+
+  return 10;
+}
+
 // 2) Parameter > Family 파라미터를 일반 함수처럼 사용할 수 있도록
